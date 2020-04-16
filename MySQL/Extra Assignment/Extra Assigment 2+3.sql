6ac50020a488ea2c9fdd5c6a05099139d428ae74
dtloan7012 <dinhthuloan1996@gmail.com>
2020-04-16 10:14:44 +0700

    Extra Assignments 4

--- MySQL/Extra Assignment/Extra Assigment 2+3.sql
+++ MySQL/Extra Assignment/Extra Assigment 2+3.sql
@@ -5,7 +5,7 @@ USE 						FresherManagement;
 
 -- Create Trainee table--
 DROP TABLE IF EXISTS Trainee;
-CREATE TABLE  Trainee (
+CREATE TABLE		Trainee (
     Trainee_id 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	Full_name			NVARCHAR(100) NOT NULL,
     Birth_day			DATE NOT NULL,
@@ -13,15 +13,15 @@ CREATE TABLE  Trainee (
     ET_IQ				TINYINT UNSIGNED NOT NULL CHECK ( ET_IQ <= 20) ,
     ET_Gmath 			TINYINT UNSIGNED NOT NULL CHECK (ET_Gmath <=20 ),
     ET_English			TINYINT UNSIGNED NOT NULL CHECK (ET_English <= 50) ,
-    Training_Class      CHAR(25) NOT NULL,
+    Training_Class      CHAR(10) NOT NULL,
     Evalution_Notes  	TEXT 
 );
 ALTER TABLE Trainee
-    ADD VTI_Account     VARCHAR(100) NOT NULL UNIQUE KEY;
+    ADD VTI_Account     VARCHAR(50) NOT NULL UNIQUE KEY;
     
 -- Question 1: 
-INSERT INTO       Trainee     (    Full_name      ,    Birth_day    ,   Gender  ,  ET_IQ  , ET_Gmath, ET_English, Training_Class , Evalution_Notes ,  VTI_Account )
-VALUES                     ( 'Nguyễn Thị Mai'    ,   '1997-11-27'     , 'Male'    ,    5    ,    5    ,     10    , 'VTI_Tester' ,'Tiếp thu nhanh' , 'mai.nguyenthi@vti.com.vn' ),    -- 1
+INSERT INTO       Trainee     (    Full_name      ,    Birth_day      ,  Gender   ,  ET_IQ  , ET_Gmath, ET_English, Training_Class , Evalution_Notes ,  VTI_Account )
+VALUES                     ( 'Nguyễn Thị Mai'     ,   '1997-11-27'    , 'Male'    ,    5    ,    5    ,     10    , 'VTI_Tester' ,'Tiếp thu nhanh' , 'mai.nguyenthi@vti.com.vn' ),    -- 1
                             ( 'Đoàn Văn Hải'      ,   '1995-03-12'    , 'Unknown' ,    10   ,    10   ,     40    , 'VTI_JAVA'   , 'Chăm học'      , 'hai.doanvan@vti.com.vn'   ),    -- 2
 							( 'Phạm Văn Minh'     ,   '1995-05-13'    , 'Female'  ,    16   ,    18   ,     30    , 'VTI_Tester' , 'Có cố gắng'    , 'minh.phamvan@vti.com.vn'  ),    -- 3
 						    ( 'Đinh Thu Trang'    ,   '1998-11-17'    , 'Male'    ,    15   ,    10   ,     40    , 'VTI_JAVA'   , 'Tiếp thu nhanh', 'trang.dinhthu@vti.com.vn' ),    -- 4
@@ -33,17 +33,16 @@ VALUES                     ( 'Nguyễn Thị Mai'    ,   '1997-11-27'     , 'Mal
                             ('Đinh Thùy Dung'     ,   '1994-11-28'    , 'Unknown' ,    19   ,    18   ,     43    , 'VTI_Tester' , 'Cần cố gắng hơn', 'dung.dinhthuy@vti.com.vn');    -- 10
     
 
-USE   		FresherManagement;
 -- Question 2: Viết lệnh để lấy ra tất cả các thực tập sinh đã vượt qua bài test đầu vào nhóm chúng thành các tháng sinh khác nhau
 
-SELECT MONTH(Birth_day), GROUP_CONCAT(Full_name)
-FROM Trainee  
-GROUP BY MONTH(Birth_day);
+SELECT 		MONTH(Birth_day), GROUP_CONCAT(Full_name)
+FROM		Trainee  
+GROUP BY 	MONTH(Birth_day);
 
 -- Question 3: Viết lệnh để lấy ra thực tập sinh có tên dài nhất, lấy ra các thông tin sau: tên, tuổi, các thông tin cơ bản (như đã được định nghĩa trong table)
-SELECT * ,LENGTH(Full_name)
-FROM     Trainee 
-WHERE    LENGTH(Full_name) = (SELECT MAX(LENGTH(Full_name)) FROM Trainee) ; 
+SELECT 		* ,LENGTH(Full_name)
+FROM    	 Trainee 
+WHERE   	 LENGTH(Full_name) = (SELECT MAX(LENGTH(Full_name)) FROM Trainee) ; 
 
 -- Question 4: Viết lệnh để lấy ra tất cả các thực tập sinh là ET, 1 ET thực tập sinh là những người 
             -- đã vượt qua bài test đầu vào và thỏa mãn số điểm như sau: 
@@ -52,19 +51,22 @@ WHERE    LENGTH(Full_name) = (SELECT MAX(LENGTH(Full_name)) FROM Trainee) ;
 			--  ET_Gmath>=8
 			--  ET_English>=18
 
-SELECT  *
-FROM Trainee
-WHERE (ET_IQ + ET_Gmath)>=20 AND ET_IQ>=8 AND ET_Gmath>=8 AND  ET_English>=18;
+SELECT 		 *
+FROM 		Trainee
+WHERE 		(ET_IQ + ET_Gmath)>=20 
+	AND 	ET_IQ>=8
+    AND 	ET_Gmath>=8 
+    AND 	ET_English>=18;
 
   -- Question 5: xóa thực tập sinh có TraineeID = 3
    
-DELETE FROM Trainee
-WHERE Trainee_id ='3' ; 
+DELETE FROM		 Trainee
+WHERE 			Trainee_id ='3' ; 
 
 -- Question 6: Thực tập sinh có TraineeID = 5 được chuyển sang lớp "2". Hãy cập nhật thông tin vào database
-   UPDATE Trainee
-   SET  Training_Class ='Lớp_2'
-   WHERE Trainee_id =3 ;
+   UPDATE 		Trainee
+   SET  		Training_Class ='Lớp_2'
+   WHERE 		Trainee_id =3 ;
 
 
 
